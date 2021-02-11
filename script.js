f27230cfa9ed240a9a11fdc343f4968c313c0deb
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-02-11 17:28:41 +0100

    Modif

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
-let div = document.createElement("img");
+
 let image1 = "image1.jpg";
 let image2 = "image2.jpg";
 let image3 = "image3.jpg";
 let image4 = "image4.jpg";
 let array = [image1, image2, image3, image4];
+let images = document.getElementsByClassName("image");
 
-document.body.append(div);
-div.className = "image";
 let x = 0;
+
+for(let image of images) {
+    image.style.backgroundImage = "url('" + array[x] + "')";
+    x++;
+}
 
 function carousel() {
     let time = setTimeout(function () {
-        div.src = array[x];
-        x++
-        if(x === array.length) {
-            x = 0;
-        }
+        left();
         carousel()
     }, 3000)
 }
 
-carousel()
+function left() {
+    images[0].animate([
+        // keyframes
+        { transform: 'translateX(-100%)' }
+    ], {
+        // timing options
+        duration: 1000,
+    });
+
+    images[1].animate([
+        // keyframes
+        { transform: 'translateX(-100%)' }
+    ], {
+        // timing options
+        duration: 1000,
+    });
+
+    images[2].animate([
+        // keyframes
+        { transform: 'translateX(-100%)' }
+    ], {
+        // timing options
+        duration: 1000,
+    });
+
+    setTimeout(function () {
+        switchImageLeft();
+    }, 1008)
+}
+carousel();
+
+function switchImageLeft() {
+    for(let image of images) {
+        for(let item of array) {
+            if(image.style.backgroundImage ===  'url("' + item + '")') {
+                let index = array.indexOf(item);
+                if(index < (array.length - 1)) {
+                    image.style.backgroundImage = "url('" + array[index + 1] + "')";
+                }
+                else {
+                    image.style.backgroundImage = "url('" + array[0] + "')";
+                }
+                break;
+            }
+        }
+    }
+}
+
