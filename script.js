992479edfa599321a8fb3e664bc88281bd4a602f
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-02-12 14:05:06 +0100

    Modif

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let image1 = "image1.jpg";
 let image2 = "image2.jpg";
 let image3 = "image3.jpg";
@@ -15,121 +14,125 @@ for(let image of images) {
     x++;
 }
 
+carousel();
+
+/**
+ * Begin carrousel
+ */
 function carousel() {
     let time = setTimeout(function () {
-        left();
+        direction("-100%", "left", 2);
         removeListener(leftGo, rightGo);
-    }, 3000)
+    }, 3000);
 
     buttonLeft.addEventListener("click", leftGo);
-
     buttonRight.addEventListener("click", rightGo);
 
+    /**
+     * Scroll left
+     */
     function leftGo() {
         if(getComputedStyle(images[1])["transform"] === ("none")) {
             clearTimeout(time);
-            right();
+            direction("-100%", "left", 2);
             removeListener(leftGo, rightGo);
         }
     }
 
+    /**
+     * Scroll right
+     */
     function rightGo() {
         if(getComputedStyle(images[1])["transform"] === ("none")) {
             clearTimeout(time);
-            left();
+            direction("+100%", "right", 0);
             removeListener(leftGo, rightGo);
         }
     }
 }
 
+/**
+ * Remove listener of button left and right
+ * @param leftGo
+ * @param rightGo
+ */
 function removeListener(leftGo, rightGo) {
     buttonLeft.removeEventListener("click", leftGo);
     buttonRight.removeEventListener("click", rightGo);
 }
 
-function left() {
-    images[0].animate([
-        { transform: 'translateX(-100%)' }
-    ], {
-        duration: 500,
-    });
-
+/**
+ * Function for animate the carrousel
+ * @param direction
+ * @param dir
+ * @param number
+ */
+function direction(direction, dir, number) {
     images[1].animate([
-        { transform: 'translateX(-100%)' }
+        { transform: 'translateX(' + direction + ')' }
     ], {
         duration: 500,
     });
 
-    images[2].animate([
-        { transform: 'translateX(-100%)' }
+    images[number].animate([
+        { transform: 'translateX(' + direction + ')' }
     ], {
         duration: 500,
     });
 
     setTimeout(function () {
-        switchImageLeft();
+        backgroundParent(number);
+        switchImage(dir);
         carousel();
     }, 500);
 }
 
-function right() {
-    images[0].animate([
-        { transform: 'translateX(+100%)' }
-    ], {
-        duration: 500,
-    });
-
-    images[1].animate([
-        { transform: 'translateX(+100%)' }
-    ], {
-        duration: 500,
-    });
-
-    images[2].animate([
-        { transform: 'translateX(+100%)' }
-    ], {
-        duration: 500,
-    });
-
-    setTimeout(function () {
-        switchImageRight();
-        carousel();
-    }, 500);
-
-}
-
-carousel();
-
-function switchImageLeft() {
+/**
+ * Function for switch images of divs
+ * @param dir
+ */
+function switchImage(dir) {
     for(let image of images) {
         for(let item of array) {
             if(image.style.backgroundImage ===  'url("' + item + '")') {
                 let index = array.indexOf(item);
-                if(index < (array.length - 1)) {
-                    image.style.backgroundImage = "url('" + array[index + 1] + "')";
+                if(dir === "left") {
+                    if(index < (array.length - 1)) {
+                        image.style.backgroundImage = "url('" + array[index + 1] + "')";
+                    }
+                    else {
+                        image.style.backgroundImage = "url('" + array[0] + "')";
+                    }
                 }
-                else {
-                    image.style.backgroundImage = "url('" + array[0] + "')";
+                else if(dir === "right"){
+                    if(index > 0) {
+                        image.style.backgroundImage = "url('" + array[index - 1] + "')";
+                    }
+                    else {
+                        image.style.backgroundImage = "url('" + array[array.length - 1] + "')";
+                    }
                 }
+
                 break;
             }
         }
     }
 }
 
-function switchImageRight() {
+/**
+ * Function for debug image switch
+ * @param image
+ */
+function backgroundParent(image) {
+    document.getElementById("carrousel").style.backgroundImage = images[image].style.backgroundImage;
     for(let image of images) {
-        for(let item of array) {
-            if(image.style.backgroundImage ===  'url("' + item + '")') {
-                let index = array.indexOf(item);
-                if(index > 0) {
-                    image.style.backgroundImage = "url('" + array[index - 1] + "')";
-                }
-                else {
-                    image.style.backgroundImage = "url('" + array[array.length - 1] + "')";
-                }
-                break;
-            }
-        }
+        image.style.display = "none";
     }
+
+    setTimeout(function() {
+        document.getElementById("carrousel").style.backgroundImage = "none";
+        for(let image of images) {
+            image.style.display = "initial";
+        }
+    }, 50);
 }
