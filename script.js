5c358c865708df7b7df4a6b19925a02ae48d6c08
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-02-12 12:35:44 +0100

    Modif

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let image4 = "image4.jpg";
 let array = [image1, image2, image3, image4];
 let images = document.getElementsByClassName("image");
 let buttonLeft = document.getElementById("buttonLeft");
-let buttonright = document.getElementById("buttonRight");
+let buttonRight = document.getElementById("buttonRight");
 
 let x = 0;
 
@@ -18,45 +18,86 @@ for(let image of images) {
 function carousel() {
     let time = setTimeout(function () {
         left();
+        removeListener(leftGo, rightGo);
     }, 3000)
 
-    buttonLeft.addEventListener("click", function () {
-        clearTimeout(time);
-        setTimeout(function () {
-            left();
-        }, 50)
+    buttonLeft.addEventListener("click", leftGo);
 
-    });
+    buttonRight.addEventListener("click", rightGo);
+
+    function leftGo() {
+        if(getComputedStyle(images[1])["transform"] === ("none")) {
+            clearTimeout(time);
+            right();
+            removeListener(leftGo, rightGo);
+        }
+    }
+
+    function rightGo() {
+        if(getComputedStyle(images[1])["transform"] === ("none")) {
+            clearTimeout(time);
+            left();
+            removeListener(leftGo, rightGo);
+        }
+    }
+}
+
+function removeListener(leftGo, rightGo) {
+    buttonLeft.removeEventListener("click", leftGo);
+    buttonRight.removeEventListener("click", rightGo);
 }
 
 function left() {
     images[0].animate([
         { transform: 'translateX(-100%)' }
     ], {
-        duration: 1000,
+        duration: 500,
     });
 
     images[1].animate([
         { transform: 'translateX(-100%)' }
     ], {
-        duration: 1000,
+        duration: 500,
     });
 
     images[2].animate([
         { transform: 'translateX(-100%)' }
     ], {
-        duration: 1000,
+        duration: 500,
     });
 
-    let timeLeft = setTimeout(function () {
+    setTimeout(function () {
         switchImageLeft();
         carousel();
-    }, 1008);
-
-    buttonLeft.addEventListener("click", function () {
-        clearTimeout(timeLeft);
-    });
+    }, 500);
 }
+
+function right() {
+    images[0].animate([
+        { transform: 'translateX(+100%)' }
+    ], {
+        duration: 500,
+    });
+
+    images[1].animate([
+        { transform: 'translateX(+100%)' }
+    ], {
+        duration: 500,
+    });
+
+    images[2].animate([
+        { transform: 'translateX(+100%)' }
+    ], {
+        duration: 500,
+    });
+
+    setTimeout(function () {
+        switchImageRight();
+        carousel();
+    }, 500);
+
+}
+
 carousel();
 
 function switchImageLeft() {
@@ -69,6 +110,23 @@ function switchImageLeft() {
                 }
                 else {
                     image.style.backgroundImage = "url('" + array[0] + "')";
+                }
+                break;
+            }
+        }
+    }
+}
+
+function switchImageRight() {
+    for(let image of images) {
+        for(let item of array) {
+            if(image.style.backgroundImage ===  'url("' + item + '")') {
+                let index = array.indexOf(item);
+                if(index > 0) {
+                    image.style.backgroundImage = "url('" + array[index - 1] + "')";
+                }
+                else {
+                    image.style.backgroundImage = "url('" + array[array.length - 1] + "')";
                 }
                 break;
             }
